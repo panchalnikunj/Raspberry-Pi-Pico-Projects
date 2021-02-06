03f250f6ea2d26a29c2ca60422bcda4926b85523
Nikunj Panchal <59598242+panchalnikunj@users.noreply.github.com>
2021-02-06 14:25:32 +0530

    Update main.py

--- Raspberry Pi Pico with 16*2 LCD Display/main.py
+++ Raspberry Pi Pico with 16*2 LCD Display/main.py
@@ -1,14 +1,9 @@
-sdasfasf
-dsfdafdaf
+from pico_i2c_lcd import I2cLcd
+from machine import I2C
+from machine import Pin
+import utime as time
 
+i2c = I2C(id=1,scl=Pin(27),sda=Pin(26),freq=100000)
+lcd = I2cLcd(i2c, 0x27, 2, 16) # LCD 16x2
 
-h
-bb
-b
-b
-b
-b
-b
-b
-
-b
+lcd.putstr('Hello World')
