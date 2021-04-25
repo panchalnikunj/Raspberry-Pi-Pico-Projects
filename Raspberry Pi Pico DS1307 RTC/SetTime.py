11d7b6da4992b050ee834cee2a0e1919e40a3a16
Nikunj Panchal <59598242+panchalnikunj@users.noreply.github.com>
2021-04-25 12:21:12 +0530

    Update SetTime.py

--- Raspberry Pi Pico DS1307 RTC/SetTime.py
+++ Raspberry Pi Pico DS1307 RTC/SetTime.py
@@ -1,1 +1,22 @@
+from machine import I2C, Pin
+from urtc import DS1307
+import utime
 
+i2c = I2C(0,scl = Pin(1),sda = Pin(0),freq = 400000)
+rtc = DS1307(i2c)
+
+year = int(input("Year : "))
+month = int(input("month (Jan --> 1 , Dec --> 12): "))
+date = int(input("date : "))
+day = int(input("day (1 --> monday , 2 --> Tuesday ... 0 --> Sunday): "))
+hour = int(input("hour (24 Hour format): "))
+minute = int(input("minute : "))
+second = int(input("second : "))
+
+now = (year,month,date,day,hour,minute,second,0)
+rtc.datetime(now)
+
+while True:
+    (year,month,date,day,hour,minute,second,p1)=rtc.datetime()
+    utime.sleep(1)
+    print(rtc.datetime())
